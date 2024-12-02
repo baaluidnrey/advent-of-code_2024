2f22c481ff4d214d2b10b1d3df823fb3467a05fd
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2024-12-03 00:09:48 +0100

    day 2  - part. 2 not working

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -20,18 +20,53 @@ int sign(int val)
     return (val>=0) ? 1 : -1;
 }
 
+int computeBadLevel(const vector<int> &v, vector<int> &pos)
+{
+    int bad_level = 0;
+    for ( int i = 0; i < v.size(); i++ )
+    {
+        int tmp_res = 1;
+
+        // all decreasing or ascending
+        if (i==0 && v.size()>2)
+            tmp_res *= ( sign(v[i+1]-v[i])==sign(v[i+2]-v[i+1]) );
+        
+        else if (i==v.size() && v.size()>2)
+            tmp_res *= ( sign(v[i]-v[i-1])==sign(v[i-1]-v[i-2]) );
+
+        else if (inRange(1,v.size()-1,i))
+            tmp_res *= ( sign(v[i]-v[i-1])==sign(v[i+1]-v[i]) );
+
+        // difference in range
+        if (i>0)
+            tmp_res *= inRange(1, 3, abs(v[i]-v[i-1]));
+
+        if (i<v.size()-1)
+            tmp_res *= inRange(1, 3, abs(v[i+1]-v[i]));
+
+        // computation of bad level
+        if(tmp_res == 0)
+        {
+            bad_level++;
+            pos.push_back(i);
+        }
+    }
+    return bad_level;
+}
+
 
 int main()
 {
 
     // puzzle variables
     string str;
-    vector<int> v;
+    vector<int> v, pos, len;
+    int bad_level;
     int res = 0;
 
 
     // open puzzle input
-    ifstream f("input.csv");
+    ifstream f("test_input.csv");
     ofstream debug_file;
     debug_file.open("debug_file.txt");
     if (!f.is_open() || !debug_file.is_open())
@@ -44,7 +79,8 @@ int main()
     // read input and put data into the arrays
     while (getline(f, str))
     {  
-        vector<int> pos, len;
+        pos.clear();    // initialization
+        len.clear();    // initialization
 
         pos.push_back(0);                           // initial position
         for (int i=1; i < str.length(); i++)
@@ -73,20 +109,23 @@ int main()
 
 
         // compute result
-        int i = 1;
-        int tmp_res = 1;
-        while ( tmp_res!=0 && i < v.size()-1 )
+        pos.clear();
+        bad_level = computeBadLevel(v, pos);
+        if (bad_level >= 1) res++;
+        else
         {
-            tmp_res *= inRange(1, 3, abs(v[i]-v[i-1]));
-            tmp_res *= inRange(1, 3, abs(v[i+1]-v[i]));
-            tmp_res *= ( sign(v[i]-v[i-1])==sign(v[i+1]-v[i]) );
-            if (debug) debug_file << "current val: " << v[i] << endl;
-            if (debug) debug_file << "---" << endl;
-            i++;
+            // v.erase(v.begin()+i);
         }
-        res += tmp_res;
-        if (debug) debug_file << "tmp_res : " << tmp_res << endl;
-        if (debug) debug_file << "---" << endl;
+
+        if (debug) 
+        {
+            debug_file << "bad_level: " << bad_level;
+            debug_file << "; pos: ";
+            for (int i = 0; i < pos.size(); i++)
+                debug_file << pos[i] << ", ";
+            debug_file << endl;
+        }
+
     }
     cout << "res part. 1 : " << res << endl;
     f.close();
