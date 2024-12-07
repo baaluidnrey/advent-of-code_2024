8c506961a3641aff4f13bba16638c9bc41608ff1
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2024-12-07 12:56:54 +0100

    day 4 - bit of cleaning before part 2

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -6,62 +6,26 @@
 using namespace std;
 
 
-int search_word(const vector<char> &v, const int width, const int height, const int pos[2], const int dec[2], const string word)
+int search_word(const vector<char> &v, const int width, const int height, const int pos[2], const int di, const int dj, const string word)
 {
     int index;
     int res = 1;
 
     for ( int i = 1; i < word.length(); i++)
     {
-        index = (pos[0] + i * dec[0]) * width + (pos[1] + i * dec[1]);
+        index = (pos[0] + i * di) * width + (pos[1] + i * dj);
 
-        if ( pos[0] + i * dec[0] < 0 || pos[0] + i * dec[0] >= height )
+        if (    ( pos[0] + i * di < 0 || pos[0] + i * di >= height )
+             || ( pos[1] + i * dj < 0 || pos[1] + i * dj >= width )
+             || ( v[index]!=word[i] )
+           )
         {
             res = 0;
-            // cerr << "out of lines" << endl;
-            break;
-        }
-
-        else if ( pos[1] + i * dec[1] < 0 || pos[1] + i * dec[1] >= width )
-        {
-            res = 0;
-            // cerr << "out of rows" << endl;
-            break;
-        }
-
-        else if ( v[index]!=word[i] )
-        {
-            res = 0;
-            // cerr << "char is not a match: " << v[index] << " != " << word[i] << endl;
             break;
         }
     }
 
-    if (res == 1) cout << "GOT A MATCH" << endl; 
-
-
-    /*
-    do 
-    {
-        i++;
-        index = (pos[0] + i * dec[0]) * width + (pos[1] + i * dec[1]);
-
-        if ( pos[0] + i * dec[0] < 0 || pos[0] + i * dec[0] > height )
-        {
-            res = 0;
-            cerr << "out of lines" << endl;
-            break;
-        }
-
-        if ( pos[1] + i * dec[1] < 0 || pos[1] + i * dec[1] > width )
-        {
-            res = 0;
-            cerr << "out of rows" << endl;
-            break;
-        }
-    }
-    while ( i<word.length()-1 && v[index]==word[i] );
-    */
+    // if (res == 1) cout << "GOT A MATCH" << endl; 
 
     return res;
 }
@@ -79,6 +43,7 @@ int main()
     string word="XMAS";
     int index = 0;
     int res = 0;
+    int tmp_res = 0;
 
 
     // open puzzle input
@@ -120,41 +85,17 @@ int main()
 
             // search XMAX
             if (v[index] == word[0])
-            {   
-
+            {
                 cout << "X is at position (" << pos[0] << ", " << pos[1] << ")" << endl;
 
-                // right
-                dec[0] = 0; dec[1] = 1;
-                res += search_word(v, width, height, pos, dec, word);
-
-                // left
-                dec[0] = 0; dec[1] = -1;
-                res += search_word(v, width, height, pos, dec, word);
-
-                // up
-                dec[0] = -1; dec[1] = 0;
-                res += search_word(v, width, height, pos, dec, word);
-            
-                // bottom
-                dec[0] = 1; dec[1] = 0;
-                res += search_word(v, width, height, pos, dec, word);
-
-                // up - right
-                dec[0] = -1; dec[1] = 1;
-                res += search_word(v, width, height, pos, dec, word);
-
-                // up - left
-                dec[0] = -1; dec[1] = -1;
-                res += search_word(v, width, height, pos, dec, word);  
-
-                // bottom - right
-                dec[0] = 1; dec[1] = 1;
-                res += search_word(v, width, height, pos, dec, word);
-
-                // bottom - left
-                dec[0] = 1; dec[1] = -1;
-                res += search_word(v, width, height, pos, dec, word);  
+                res += search_word(v, width, height, pos, 0, 1, word);      // right
+                res += search_word(v, width, height, pos, 0, -1, word);     // left
+                res += search_word(v, width, height, pos, -1, 0, word);     // up
+                res += search_word(v, width, height, pos, 1, 0, word);      // bottom
+                res += search_word(v, width, height, pos, -1, 1, word);     // up - right                
+                res += search_word(v, width, height, pos, -1, -1, word);    // up - left
+                res += search_word(v, width, height, pos, 1, 1, word);      // bottom - right
+                res += search_word(v, width, height, pos, 1, -1, word);     // bottom - left
             }
         }
     }
