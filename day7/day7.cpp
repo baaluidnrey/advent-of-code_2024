1e12ff2dc38d07d6d6e04bfb39a5e8f628139815
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2024-12-08 22:35:46 +0100

    day 7 - part. 1 OK

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -14,13 +14,13 @@ int main()
     // puzzle variables
     string str;
     int pos_start, pos_end;
-    int test_value;
-    int value, tmp_value;
-    vector<int> numbers;
-    int res = 0;
+    long test_value;
+    long value, tmp_value;
+    vector<long> numbers;
+    long res = 0;
 
     // open puzzle input
-    ifstream f("test_input.txt");
+    ifstream f("input.txt");
     if (!f.is_open())
     {
         cerr << "Error opening the file!";
@@ -31,7 +31,7 @@ int main()
     {
         // extract the test_value
         pos_end = str.find(":");
-        test_value = stoi(str.substr(0, pos_end));
+        test_value = stol(str.substr(0, pos_end));
 
         // extract the numbers
         numbers.clear();
@@ -42,13 +42,13 @@ int main()
             pos_end = str.find(" ", pos_start);
             if ( pos_end!=string::npos )
             {
-                numbers.push_back(stoi(str.substr(pos_start, pos_end)));
+                numbers.push_back(stol(str.substr(pos_start, pos_end)));
                 pos_start = pos_end+1;
             }
         }
         while ( pos_end!=string::npos );
 
-        if (pos_start > 0)  numbers.push_back(stoi(str.substr(pos_start)));
+        if (pos_start > 0)  numbers.push_back(stol(str.substr(pos_start)));
 
         // print to check
         if (debug)
@@ -62,8 +62,7 @@ int main()
         // search if a combination of 'x' and '+' can make the equation match the test_value
         for (int i=0; i<pow(2,numbers.size()-1); i++)
         {
-            value = 0;
-            tmp_value = numbers[0];
+            value = numbers[0];
             if (debug)  cout << numbers[0];
 
             for (int j=1; j<numbers.size(); j++)
@@ -72,25 +71,23 @@ int main()
 
                 if ((i>>(j-1))&0x1)     // multiplication
                 {
-                    tmp_value*=numbers[j];
+                    value *= numbers[j];
                     if (debug)  cout << 'X';
                 }
                 
                 else                    // addition
                 {
-                    value += tmp_value;
-                    tmp_value = numbers[j];
+                    value += numbers[j];
                     if (debug)  cout << '+';
                 }
-                cout << numbers[j];
+                if (debug)  cout << numbers[j];
             }
-            value += tmp_value;
             if (debug)  cout << " = " << value << endl;
 
             // add the test_value to the result if it is equal to the result of the equation
             if (value == test_value)    
             {
-                cout << "add value to res." << endl;
+                cout << "add value to res: " << res << endl;
                 res+= test_value;
                 break;
             }
