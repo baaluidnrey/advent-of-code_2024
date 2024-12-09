06ce70271380abb0c6032d6205b06ad9b8f2f7eb
Aline Baudry <aline.baudry@sorbonne-universite.fr>
2024-12-09 21:30:34 +0100

    day 9 - part2. OK

--- day9/day9.cpp
+++ day9/day9.cpp
@@ -61,12 +61,19 @@ int main()
         if (debug) 
         {   
             cout << "b: ";
-            for (int i : b) cout << i << " ";
+            for (int i : b)
+            {
+                if (i==val_space) cout << '.';
+                else cout << i;
+            }
             cout << endl;
         }
 
 
         // 3. do the permutations
+
+        // part 1.
+        /*
         for (int i = b.size()-1; i>=0; i--)
         {
             if (b[i]!=val_space)
@@ -90,7 +97,50 @@ int main()
                 cout << endl;
             }
         }
+        */
 
+        // part. 2
+        int val = 0;
+        int i = b.size()-1;
+        int j, k, l;
+        do
+        {
+            // current value and position
+            val = b[i];
+            j = i;
+            do {i--;}
+            while(b[i]==val); // stops when the value changes, current value between (i+1) and j
+
+            // we then need to find a block of size (j-(i+1))+1 = (j-i)
+            k = 0; l = 0;
+            while ( k < i+1 && l != (j-i) )
+            {
+                // compute length of space
+                if (b[k] == val_space) l++;
+                else l=0;
+                k++;
+            }
+
+            // a space has been found between (k-1) and (k-l)
+            if (l == (j-i))
+            {
+                for (int index = (k-1); index >= (k-l); index--)    b[index] = val;
+                for (int index = i+1; index <=j; index++)           b[index] = val_space;
+            }
+
+        if (debug) 
+        {   
+            cout << "b: ";
+            for (int i : b)
+            {
+                if (i==val_space) cout << '.';
+                else cout << i;
+            }
+            cout << endl;
+        }
+
+        } while (i>0);
+        
 
         // 4. compute the checksum
         res = 0;
